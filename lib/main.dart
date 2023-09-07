8fdf0b91dc02665a6df573454685aae7454dfc4f
Joshua Mather <jmather0707@gmail.com>
2023-09-07 11:30:16 +0100

    Geolocation background

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:english_words/english_words.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:background_location/background_location.dart';
 
 void main() {
   runApp(MyApp());
@@ -61,41 +65,63 @@ class MyAppState extends ChangeNotifier {
   // Future<Position>
 
   void _determinePosition() async {
-  bool serviceEnabled;
-  LocationPermission permission;
+    bool serviceEnabled;
+    LocationPermission permission;
 
-  // Test if location services are enabled.
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    // Location services are not enabled don't continue
-    // accessing the position and request users of the 
-    // App to enable the location services.
-    return Future.error('Location services are disabled.');
-  }
-
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      // Permissions are denied, next time you could try
-      // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale 
-      // returned true. According to Android guidelines
-      // your App should show an explanatory UI now.
-      return Future.error('Location permissions are denied');
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the 
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
     }
-  }
-  
-  if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately. 
-    return Future.error(
-      'Location permissions are permanently denied, we cannot request permissions.');
-  } 
 
-  // When we reach here, permissions are granted and we can
-  // continue accessing the position of the device.
-  position = await Geolocator.getCurrentPosition();
-}
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale 
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+    
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately. 
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+    } 
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    position = await Geolocator.getCurrentPosition();
+  }
+
+  backgroundPositionStart() {
+    BackgroundLocation.setAndroidNotification(
+      title: "Background Location",
+      message: "Background location is active",
+      icon: "@mipmap/ic_launcher",
+    );
+
+    BackgroundLocation.setAndroidConfiguration(1000);
+
+    BackgroundLocation.startLocationService();
+
+    BackgroundLocation.getLocationUpdates((location) {
+      print(location.longitude);
+    });
+  }
+
+  backgroundPositionStop() {
+    BackgroundLocation.stopLocationService();
+  }
+
+ 
 }
 
 class MyHomePage extends StatefulWidget {
@@ -397,6 +423,18 @@ class _LocationPageState extends State<LocationPage> {
                 appState._determinePosition();
               },
               child: Text('Get Position'),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                appState.backgroundPositionStart();
+              },
+              child: Text('Bakground Position Start'),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                appState.backgroundPositionStop();
+              },
+              child: Text('Bakground Position Stop'),
           ),
         ]
         
